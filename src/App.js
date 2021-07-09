4efaa565044ec1c656d68cbc15816faec1e4c083
Damian Tokarczyk <damian.tokarczyk@gmail.com>
2021-07-09 10:15:58 +0200

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Form = styled.form`
+  max-width: 600px;
+  margin: 0 auto;
+  label {
+    margin-right: 1rem;
+  }
+`;
+
+const API_ENDPOINT = "https://pokeapi.co/api/v2/pokemon/";
 
 function App() {
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    weight: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [valueInput, setValueInput] = useState("");
+
+  const handleChangeInput = (e) => {
+    setValueInput(e.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    setError("");
+    setPokemon("");
+    axios
+      .get(`${API_ENDPOINT}${valueInput}`)
+      .then(({ data }) => {
+        const { name, weight } = data;
+
+        setPokemon({
+          name,
+          weight,
+        });
+        setLoading(false);
+      })
+      .catch((e) => {
+        setError(e.response.data);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon sercher</h1>
+      <Form onSubmit={handleSearch}>
+        <label>Name of pokemon</label>
+
+        <input type="text" onChange={handleChangeInput} />
+        <input type="submit" value="Znajdz" />
+      </Form>
+
+      {loading && <div>Ładuję...</div>}
+      {pokemon.name && pokemon.weight && (
+        <div>
+          {pokemon.name}, {pokemon.weight}
+        </div>
+      )}
+      {error && <div>{error}</div>}
     </div>
   );
 }
